364fef9c6b0d3625f3c28c3003c301da52a427c1
jesuspol720 <jesuspol72020@gmail.com>
2023-07-11 12:41:20 -0500

    segunda actividad clase 07/07/2023

---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+
+  slides = [
+    {
+     title: "MT 15",
+     img: "assets/images/mt15.jpg",
+     icon: "fitness-outline",
+     description:"Diseñada con todo el ADN de la gama MT,la MT-15 es una motocicleta de diseño avanzado,aceleración agresiva y diseño ágil. Además,cuenta con tecnología avanzada te mantieneconectado con tu lado oscuro a través de la aplicación Y-Connect."
+    },
+
+    {
+     title: "MT 03",
+     img: "assets/images/mt03.png",
+     icon: "fitness-outline",
+     description:"Diseñada con todo el ADN de la gama MT,la MT-03 es una motocicleta de diseño avanzado,aceleración agresiva y diseño ágil. Además,cuenta con tecnología avanzada te mantieneconectado con tu lado oscuro a través de la aplicación Y-Connect."
+    },
+
+    {
+     title: "MT 07",
+     img: "assets/images/mt07.jpg",
+     icon: "fitness-outline",
+     description:"Diseñada con todo el ADN de la gama MT,la MT-07 es una motocicleta de diseño avanzado,aceleración agresiva y diseño ágil. Además,cuenta con tecnología avanzada te mantieneconectado con tu lado oscuro a través de la aplicación Y-Connect."
+    },
+
+    {
+     title: "MT 09",
+     img: "assets/images/mt09.jpg",
+     icon: "fitness-outline",
+     description:"Diseñada con todo el ADN de la gama MT,la MT-09 es una motocicleta de diseño avanzado,aceleración agresiva y diseño ágil. Además,cuenta con tecnología avanzada te mantieneconectado con tu lado oscuro a través de la aplicación Y-Connect."
+    },
+
+    {
+     title: "MT 10",
+     img: "assets/images/mt10.jpg",
+     icon: "fitness-outline",
+     description:"Diseñada con todo el ADN de la gama MT,la MT-10 es una motocicleta de diseño avanzado,aceleración agresiva y diseño ágil. Además,cuenta con tecnología avanzada te mantieneconectado con tu lado oscuro a través de la aplicación Y-Connect."
+    }
+
+   ]
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+  
+  close() {
+   this.router.navigateByUrl("/home");
+  }
+}
